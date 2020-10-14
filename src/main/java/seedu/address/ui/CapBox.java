036c4b6729413365c67877036acf9de7fc8b5add
kunnan <hongkunnan1997@gmail.com>
2020-10-14 17:00:52 +0800

    Edit checkstyle errors

--- src/main/java/seedu/address/ui/CapBox.java
+++ src/main/java/seedu/address/ui/CapBox.java
@@ -1,26 +1,37 @@
 package seedu.address.ui;
 
+import static java.util.Objects.requireNonNull;
+
 import javafx.fxml.FXML;
 import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
-import static java.util.Objects.requireNonNull;
-
+/**
+ * The UI component that is responsible for displaying CAP.
+ */
 public class CapBox extends UiPart<Region> {
     private static final String FXML = "capBox.fxml";
 
     @FXML
-    private Text currentCAPDisplay;
+    private Text currentCapDisplay;
 
-    public CapBox(String currentCAP) {
+    /**
+     * Creates a {@code CapBox} with the given {@code currentCap}.
+     */
+    public CapBox(String currentCap) {
         super(FXML);
-        setCAPDisplay(currentCAP);
-        currentCAPDisplay.setFill(Color.WHITE);
+        setCapDisplay(currentCap);
+        currentCapDisplay.setFill(Color.WHITE);
     }
 
-    public void setCAPDisplay(String currentCAP) {
-        requireNonNull(currentCAP);
-        currentCAPDisplay.setText("Current CAP: " + currentCAP);
+    /**
+     * Set the display CAP's value
+     *
+     * @param currentCap
+     */
+    public void setCapDisplay(String currentCap) {
+        requireNonNull(currentCap);
+        currentCapDisplay.setText("Current CAP: " + currentCap);
     }
 }
