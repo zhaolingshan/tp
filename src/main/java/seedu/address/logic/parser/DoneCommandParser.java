b25bc6fe58fa3e609d43978a1362ca81d53b86cb
zhaolingshan <e0425857@u.nus.edu>
2020-10-21 19:02:40 +0800

    Edit DoneCommandParser

--- src/main/java/seedu/address/logic/parser/DoneCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DoneCommandParser.java
@@ -1,9 +1,11 @@
 package seedu.address.logic.parser;
 
+import seedu.address.commons.core.Messages;
 import seedu.address.logic.commands.DoneCommand;
 import seedu.address.logic.commands.StartCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.semester.Semester;
+import seedu.address.model.semester.SemesterManager;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
@@ -12,12 +14,17 @@ public class DoneCommandParser implements Parser<DoneCommand> {
     @Override
     public DoneCommand parse(String userInput) throws ParseException {
         requireNonNull(userInput);
+        SemesterManager semesterManager = SemesterManager.getInstance();
+        Semester semester = semesterManager.getCurrentSemester();
+        if (semester == Semester.NA) {
+            throw new ParseException(Messages.MESSAGE_INVALID_DONE_COMMAND);
+        }
         try {
-            Semester semester = ParserUtil.parseSemester(userInput);
-            return new DoneCommand(semester);
+            Semester sem = ParserUtil.parseSemester(userInput);
+            return new DoneCommand(sem);
         } catch (ParseException pe) {
             throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, StartCommand.MESSAGE_USAGE), pe);
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DoneCommand.MESSAGE_USAGE), pe);
         }
     }
 }
