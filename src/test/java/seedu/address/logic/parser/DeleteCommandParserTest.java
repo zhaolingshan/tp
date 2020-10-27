e88db22f146e76831fd7195f90b9bd28ad81cdf9
zhaolingshan <e0425857@u.nus.edu>
2020-10-27 21:17:34 +0800

    Add tests in DeleteCommandParserTest for invalid and valid semesters

--- src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
@@ -1,11 +1,19 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.logic.commands.CommandTestUtil.VALID_MODULAR_CREDIT;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalModules.COM_ORG;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.core.Messages;
 import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.model.module.Module;
+import seedu.address.model.module.ModuleName;
+import seedu.address.model.semester.Semester;
+import seedu.address.testutil.ModuleBuilder;
 
 /**
  * As we are only doing white-box testing, our test cases do not cover path variations
@@ -17,10 +25,27 @@ import seedu.address.logic.commands.DeleteCommand;
 public class DeleteCommandParserTest {
 
     private DeleteCommandParser parser = new DeleteCommandParser();
+    private final ModuleName moduleName = COM_ORG.getModuleName();
+    private final String userInputModuleName = "CS2100";
 
     @Test
     public void parse_validArgs_returnsDeleteCommand() {
-        System.out.println(new DeleteCommand(COM_ORG.getModuleName()));
-        assertParseSuccess(parser, "CS2100", new DeleteCommand(COM_ORG.getModuleName()));
+        System.out.println(new DeleteCommand(moduleName));
+        assertParseSuccess(parser, userInputModuleName, new DeleteCommand(moduleName));
+    }
+
+    @Test
+    public void parse_invalidSemester_failure() {
+        Semester invalidSemester = Semester.NA;
+        assertParseFailure(parser, invalidSemester.toString(), Messages.MESSAGE_INVALID_COMMAND_SEQUENCE);
+    }
+
+    @Test
+    public void parse_validSemester_success() {
+        Semester validSemester = Semester.Y4S1;
+        Module expectedModule = new ModuleBuilder(COM_ORG).withTags(VALID_TAG_FRIEND)
+                .withModularCredit(VALID_MODULAR_CREDIT).withSemester(validSemester.toString()).build();
+        DeleteCommand deleteCommand = new DeleteCommand(expectedModule.getModuleName());
+        assertParseSuccess(parser, userInputModuleName, deleteCommand);
     }
 }
