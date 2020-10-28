57a83c48dc34817587a2e2dbddebe6ef4aebbe05
zhaolingshan <e0425857@u.nus.edu>
2020-10-28 22:35:08 +0800

    Add JavaDocs for HelpCommandParser

--- src/main/java/seedu/address/logic/parser/HelpCommandParser.java
+++ src/main/java/seedu/address/logic/parser/HelpCommandParser.java
@@ -4,7 +4,17 @@ import seedu.address.commons.core.Messages;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+/**
+ * Parses input arguments and creates a new HelpCommand object
+ */
 public class HelpCommandParser implements Parser<HelpCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the HelpCommand
+     * and returns a HelpCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format.
+     */
     @Override
     public HelpCommand parse(String userInput) throws ParseException {
         String trimmedInput = userInput.trim();
