31c7567ef7d01142d475ed4efc5120fdc32d8cc2
zhaolingshan <e0425857@u.nus.edu>
2020-11-04 18:37:59 +0800

    Edit ProgressCommandParserTest

--- src/test/java/seedu/address/logic/parser/ProgressCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ProgressCommandParserTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_INPUT_FOR_ONE_WORD_COMMAND;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DOUBLE_DEGREE;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
@@ -12,8 +13,6 @@ import seedu.address.logic.commands.ProgressCommand;
 public class ProgressCommandParserTest {
 
     private static final boolean IS_DDP = true;
-
-    private static final String INVALID_INPUT = "invalid input";
 
     private ProgressCommandParser parser = new ProgressCommandParser();
 
@@ -29,13 +28,13 @@ public class ProgressCommandParserTest {
 
     @Test
     public void parse_invalidInput_failure() {
-        assertParseFailure(parser, INVALID_INPUT, String.format(
+        assertParseFailure(parser, INVALID_INPUT_FOR_ONE_WORD_COMMAND, String.format(
                 MESSAGE_INVALID_COMMAND_FORMAT, ProgressCommand.MESSAGE_USAGE));
     }
 
     @Test
     public void parse_ddpWithInvalidInput_failure() {
-        String userInput = PREFIX_DOUBLE_DEGREE + " " + INVALID_INPUT;
+        String userInput = PREFIX_DOUBLE_DEGREE + " " + INVALID_INPUT_FOR_ONE_WORD_COMMAND;
         assertParseFailure(parser, userInput, String.format(
                 MESSAGE_INVALID_COMMAND_FORMAT, ProgressCommand.MESSAGE_USAGE));
     }
