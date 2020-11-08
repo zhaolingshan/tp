8b54594a9df8c98f8762ff935e0e84eaa8c33da3
kunnan <hongkunnan1997@gmail.com>
2020-11-09 02:59:38 +0800

    remove 3rd party

--- src/test/java/seedu/address/ui/ResultDisplayTest.java
+++ src/test/java/seedu/address/ui/ResultDisplayTest.java
@@ -1,46 +1,46 @@
-package seedu.address.ui;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import java.io.IOException;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.testfx.api.FxRobot;
-import org.testfx.framework.junit5.ApplicationExtension;
-import org.testfx.framework.junit5.Start;
-
-import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-
-@ExtendWith(ApplicationExtension.class)
-class ResultDisplayTest {
-
-    private ResultDisplay resultDisplay;
-    /**
-     * Will be called with {@code @Before} semantics, i. e. before each test method.
-     *
-     * @param stage - Will be injected by the test runner.
-     */
-    @Start
-    private void start(Stage stage) throws IOException {
-        resultDisplay = new ResultDisplay();
-        stage.setScene(new Scene(new AnchorPane(resultDisplay.getRoot()), 100, 100));
-        stage.show();
-    }
-
-    /**
-     * @param robot - Will be injected by the test runner.
-     */
-    @Test
-    void result_display_test(FxRobot robot) {
-        TextArea textArea = robot.lookup("#resultDisplay").queryAs(TextArea.class);
-        assertNotNull(textArea);
-
-        resultDisplay.setFeedbackToUser("TEST");
-        assertEquals("TEST", textArea.getText());
-    }
-}
+//package seedu.address.ui;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertNotNull;
+//
+//import java.io.IOException;
+//
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.testfx.api.FxRobot;
+//import org.testfx.framework.junit5.ApplicationExtension;
+//import org.testfx.framework.junit5.Start;
+//
+//import javafx.scene.Scene;
+//import javafx.scene.control.TextArea;
+//import javafx.scene.layout.AnchorPane;
+//import javafx.stage.Stage;
+//
+//@ExtendWith(ApplicationExtension.class)
+//class ResultDisplayTest {
+//
+//    private ResultDisplay resultDisplay;
+//    /**
+//     * Will be called with {@code @Before} semantics, i. e. before each test method.
+//     *
+//     * @param stage - Will be injected by the test runner.
+//     */
+//    @Start
+//    public void start(Stage stage) throws IOException {
+//        resultDisplay = new ResultDisplay();
+//        stage.setScene(new Scene(new AnchorPane(resultDisplay.getRoot()), 100, 100));
+//        stage.show();
+//    }
+//
+//    /**
+//     * @param robot - Will be injected by the test runner.
+//     */
+//    @Test
+//    void result_display_test(FxRobot robot) {
+//        TextArea textArea = robot.lookup("#resultDisplay").queryAs(TextArea.class);
+//        assertNotNull(textArea);
+//
+//        resultDisplay.setFeedbackToUser("TEST");
+//        assertEquals("TEST", textArea.getText());
+//    }
+//}
