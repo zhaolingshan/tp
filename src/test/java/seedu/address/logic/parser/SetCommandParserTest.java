7196806e40d971ce8f007fabffd26998bed14501
pongzers <you@example.com>
2020-11-08 19:49:53 +0800

    Fix tests

--- src/test/java/seedu/address/logic/parser/SetCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SetCommandParserTest.java
@@ -49,7 +49,7 @@ public class SetCommandParserTest {
     @Test
     public void parse_invalidGoalTarget_failure() {
         // out of range
-        assertParseFailure(parser, SetCommand.COMMAND_WORD + WHITESPACE + PREFIX_SET_GOAL
+        assertParseFailure(parser, " " + PREFIX_SET_GOAL
                 + INVALID_GOAL_TARGET, GoalTarget.MESSAGE_CONSTRAINTS);
     }
 }
