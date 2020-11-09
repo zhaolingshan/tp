7f11d702dd7313b7513c1d0c5fa1083bddd966aa
zhaolingshan <e0425857@u.nus.edu>
2020-11-09 16:51:09 +0800

    Fix CheckStyle

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -8,7 +8,7 @@ public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_MODULE_CANNOT_BE_SU = "%1$s cannot be S/U-ed!";
-    public static final String MESSAGE_INVALID_MODULE_DISPLAYED_NAME = "Please enter the module code of a module" 
+    public static final String MESSAGE_INVALID_MODULE_DISPLAYED_NAME = "Please enter the module code of a module"
             + " which is in the semester you are currently editing.";
     public static final String MESSAGE_MODULES_LISTED_OVERVIEW = "%1$d modules listed!";
     public static final String MESSAGE_NO_MODULES_FOUND = "No modules were found!";
